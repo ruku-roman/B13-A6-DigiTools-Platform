4d8715277215b64586e33752b63b3e795661aee4
Md Rukunuzzaman <ruku.roman@gmail.com>
2026-04-01 23:04:09 +0600

    Update Some data

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <footer className="bg-linear-to-r from-indigo-500 to-purple-600 py-24 px-6 text-center text-white">
+    <footer className="bg-linear-to-r from-indigo-500 to-purple-500 py-24 px-6 text-center text-white">
         <div className="max-w-4xl mx-auto">
         <h2 className="text-5xl font-extrabold mb-6 leading-tight">
             Ready To Transform Your Workflow?
@@ -12,7 +12,7 @@ const Footer = () => {
             Start your free trial today.
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4 mb-8">
-            <button className="btn bg-white text-indigo-600 hover:bg-gray-100 border-none px-10 h-16 rounded-full text-lg font-bold shadow-xl">
+            <button className="btn bg-white text-indigo-500 hover:bg-gray-100 border-none px-10 h-16 rounded-full text-lg font-bold shadow-xl">
             Explore Products
             </button>
             <button className="btn btn-outline border-2 border-white text-white hover:bg-white hover:text-indigo-600 px-10 h-16 rounded-full text-lg font-bold">
